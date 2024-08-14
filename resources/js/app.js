5ac4cc455e0e0a11af0ce4e06ea997939c7e1fd8
marcosherna <marcosxdf6@gmail.com>
2024-08-13 21:53:15 -0600

    delete motion

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,8 +9,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import { custon_presets } from './theme';
-import ToastService from 'primevue/toastservice';
-import { MotionPlugin } from '@vueuse/motion'
+import ToastService from 'primevue/toastservice'; 
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'; 
 
@@ -28,8 +27,7 @@ createInertiaApp({
                     options : {
                         darkModeSelector: 'system',
                     }
-            }})
-            .use(MotionPlugin)
+            }}) 
             .use(ZiggyVue)
             .mount(el);
     },
