542d7fe16b19d2b2af66a9bb670a6d27833cc8e7
hilderbran <145211893+HilderBran@users.noreply.github.com>
2024-08-20 11:46:02 -0600

    Modelo, contrador, migracion y endpoint de tabla Suppliers

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\SupplierController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::controller(SupplierController::class)->group(function () {
+    Route::get('/suppliers', 'index');
+    Route::post('/suppliers', 'store');
+    Route::put('/suppliers/{id}', 'update');
+    Route::delete('/suppliers/{id}', 'destroy');
+    Route::patch('/suppliers/{id}', 'updateStatus');
+});
