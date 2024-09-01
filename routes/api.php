3654e933f82f4bd80049fd28509c0d4536748968
hilderbran <145211893+HilderBran@users.noreply.github.com>
2024-08-31 22:36:47 -0600

    Estado de producto y compra, con sus respectivas migraciones, modelos, request y rutas

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\PurchasestatusController;
+use App\Http\Controllers\ProductstatusController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +16,20 @@ Route::controller(SupplierController::class)->group(function () {
     Route::put('/suppliers/{id}', 'update');
     Route::delete('/suppliers/{id}', 'destroy');
     Route::patch('/suppliers/{id}', 'updateStatus');
+});
+
+Route::controller(PurchasestatusController::class)->group(function () {
+    Route::get('/purchasestatuses', 'index');
+    Route::post('/purchasestatuses', 'store');
+    Route::put('/purchasestatuses/{id}', 'update');
+    Route::delete('/purchasestatuses/{id}', 'destroy');
+    Route::patch('/purchasestatuses/{id}', 'updateStatus');
+});
+
+Route::controller(ProductstatusController::class)->group(function () {
+    Route::get('/productstatuses', 'index');
+    Route::post('/productstatuses', 'store');
+    Route::put('/productstatuses/{id}', 'update');
+    Route::delete('/productstatuses/{id}', 'destroy');
+    Route::patch('/productstatuses/{id}', 'updateStatus');
 });
